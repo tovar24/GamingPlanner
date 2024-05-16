fdf2f299fb4484a1f8f876de0bd1b941a9be0120
tovar24 <cristian.t.12@hotmail.com>
2024-05-16 16:21:44 +0200

    Update BBDD

--- db/gaming_planner.sql
+++ db/gaming_planner.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 10-05-2024 a las 02:04:48
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+-- Tiempo de generación: 16-05-2024 a las 16:19:45
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,113 +24,40 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `activity`
+-- Estructura de tabla para la tabla `activities`
 --
 
-CREATE TABLE `activity` (
+CREATE TABLE `activities` (
   `id` int(11) NOT NULL,
-  `name` varchar(20) NOT NULL
+  `date` date NOT NULL,
+  `idTipeAct` int(11) NOT NULL,
+  `idTeam` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `activity`
---
-
-INSERT INTO `activity` (`id`, `name`) VALUES
-(1, 'VOD'),
-(2, 'Teórico'),
-(3, 'PRACC');
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `daily_activities`
+-- Estructura de tabla para la tabla `game`
 --
 
-CREATE TABLE `daily_activities` (
+CREATE TABLE `game` (
   `id` int(11) NOT NULL,
-  `idDay` int(11) NOT NULL,
-  `idActivity` int(11) NOT NULL
+  `date` date NOT NULL,
+  `result` varchar(10) DEFAULT NULL,
+  `idTournament` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `daily_activities`
---
-
-INSERT INTO `daily_activities` (`id`, `idDay`, `idActivity`) VALUES
-(1, 1, 1),
-(2, 1, 2);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `day`
+-- Estructura de tabla para la tabla `game_team`
 --
 
-CREATE TABLE `day` (
+CREATE TABLE `game_team` (
   `id` int(11) NOT NULL,
-  `name` varchar(20) NOT NULL
+  `idGame` int(11) NOT NULL,
+  `idTeam` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `day`
---
-
-INSERT INTO `day` (`id`, `name`) VALUES
-(1, 'Lunes'),
-(2, 'Martes'),
-(3, 'Miércoles'),
-(4, 'Jueves'),
-(5, 'Viernes');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `month`
---
-
-CREATE TABLE `month` (
-  `id` int(11) NOT NULL,
-  `name` varchar(15) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `month`
---
-
-INSERT INTO `month` (`id`, `name`) VALUES
-(1, 'Enero'),
-(2, 'Febrero'),
-(3, 'Marzo'),
-(4, 'Abril'),
-(5, 'Mayo'),
-(6, 'Junio'),
-(7, 'Julio'),
-(8, 'Agosto'),
-(9, 'Septiembre'),
-(10, 'Octubre'),
-(11, 'Noviembre'),
-(12, 'Diciembre');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `planner`
---
-
-CREATE TABLE `planner` (
-  `id` int(11) NOT NULL,
-  `idMonth` int(11) NOT NULL,
-  `idWeekDay` int(11) NOT NULL,
-  `idDailyAct` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `planner`
---
-
-INSERT INTO `planner` (`id`, `idMonth`, `idWeekDay`, `idDailyAct`) VALUES
-(1, 5, 3, 1);
 
 -- --------------------------------------------------------
 
@@ -162,19 +89,38 @@ INSERT INTO `rol` (`id`, `rol`, `privileges`) VALUES
 
 CREATE TABLE `team` (
   `id` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `idPlanner` int(11) DEFAULT NULL
+  `name` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `team`
 --
 
-INSERT INTO `team` (`id`, `name`, `idPlanner`) VALUES
-(1, 'CROW DYNASTY', NULL),
-(2, 'FUSYON', NULL),
-(3, 'KRÜ', NULL),
-(4, 'SENTINELS', NULL);
+INSERT INTO `team` (`id`, `name`) VALUES
+(1, 'CROW DYNASTY'),
+(2, 'FUSYON'),
+(3, 'KRÜ'),
+(4, 'SENTINELS');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipe_activity`
+--
+
+CREATE TABLE `tipe_activity` (
+  `id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `tipe_activity`
+--
+
+INSERT INTO `tipe_activity` (`id`, `name`) VALUES
+(1, 'VOD'),
+(2, 'Teórico'),
+(3, 'PRACC');
 
 -- --------------------------------------------------------
 
@@ -185,17 +131,18 @@ INSERT INTO `team` (`id`, `name`, `idPlanner`) VALUES
 CREATE TABLE `tournament` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
-  `url` varchar(255) DEFAULT NULL
+  `url` varchar(255) DEFAULT NULL,
+  `idGame` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `tournament`
 --
 
-INSERT INTO `tournament` (`id`, `name`, `url`) VALUES
-(1, 'Circuito Tormenta', NULL),
-(2, 'Liga Radiante', NULL),
-(3, 'Clutch Series', 'https://circuitotormenta.com/landing/clutch-series');
+INSERT INTO `tournament` (`id`, `name`, `url`, `idGame`) VALUES
+(1, 'Circuito Tormenta', NULL, NULL),
+(2, 'Liga Radiante', NULL, NULL),
+(3, 'Clutch Series', 'https://circuitotormenta.com/landing/clutch-series', NULL);
 
 -- --------------------------------------------------------
 
@@ -254,104 +201,29 @@ INSERT INTO `users_token` (`id`, `idUser`, `token`, `fecha`) VALUES
 (1, 1, 'dba672c0c7159a43cc68864e2d888a02', '2024-04-27'),
 (2, 2, 'fbb3681913dbc8243d8a49ce4c6568d8', '2024-04-27');
 
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `week`
---
-
-CREATE TABLE `week` (
-  `id` int(11) NOT NULL,
-  `name` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `week`
---
-
-INSERT INTO `week` (`id`, `name`) VALUES
-(1, 'Semana 1'),
-(2, 'Semana 2'),
-(3, 'Semana 3'),
-(4, 'Semana 4');
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `week_day`
---
-
-CREATE TABLE `week_day` (
-  `id` int(11) NOT NULL,
-  `name` varchar(15) NOT NULL,
-  `idWeek` int(11) NOT NULL,
-  `idDay` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Volcado de datos para la tabla `week_day`
---
-
-INSERT INTO `week_day` (`id`, `name`, `idWeek`, `idDay`) VALUES
-(3, 'S1Lunes', 1, 1),
-(5, 'S1Martes', 1, 2),
-(6, 'S1Miercoles', 1, 3),
-(7, 'S1Jueves', 1, 4),
-(8, 'S1Viernes', 1, 5),
-(9, 'S2Lunes', 2, 1),
-(10, 'S2Martes', 2, 2),
-(11, 'S2Miercoles', 2, 3),
-(12, 'S2Jueves', 2, 4),
-(13, 'S2Viernes', 2, 5),
-(14, 'S3Lunes', 3, 1),
-(15, 'S3Martes', 3, 2),
-(16, 'S3Miercoles', 3, 3),
-(17, 'S3Jueves', 3, 4),
-(18, 'S3Viernes', 3, 5),
-(19, 'S4Lunes', 4, 1),
-(20, 'S4Martes', 4, 2),
-(21, 'S4Miercoles', 4, 3),
-(22, 'S4Jueves', 4, 4),
-(23, 'S4Viernes', 4, 5);
-
 --
 -- Índices para tablas volcadas
 --
 
 --
--- Indices de la tabla `activity`
+-- Indices de la tabla `activities`
 --
-ALTER TABLE `activity`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `daily_activities`
---
-ALTER TABLE `daily_activities`
+ALTER TABLE `activities`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `idDay` (`idDay`) USING BTREE,
-  ADD KEY `idActivity` (`idActivity`) USING BTREE;
+  ADD KEY `fk_tipeAct_1` (`idTipeAct`),
+  ADD KEY `fk_teamID_2` (`idTeam`);
 
 --
--- Indices de la tabla `day`
+-- Indices de la tabla `game`
 --
-ALTER TABLE `day`
+ALTER TABLE `game`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `month`
+-- Indices de la tabla `game_team`
 --
-ALTER TABLE `month`
+ALTER TABLE `game_team`
   ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `planner`
---
-ALTER TABLE `planner`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `idMonth` (`idMonth`) USING BTREE,
-  ADD KEY `idDailyAct` (`idDailyAct`) USING BTREE,
-  ADD KEY `idWeek` (`idWeekDay`) USING BTREE;
 
 --
 -- Indices de la tabla `rol`
@@ -363,14 +235,20 @@ ALTER TABLE `rol`
 -- Indices de la tabla `team`
 --
 ALTER TABLE `team`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `idPlanner` (`idPlanner`) USING BTREE;
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `tipe_activity`
+--
+ALTER TABLE `tipe_activity`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `tournament`
 --
 ALTER TABLE `tournament`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_gameID_1` (`idGame`);
 
 --
 -- Indices de la tabla `tournament_team`
@@ -396,51 +274,26 @@ ALTER TABLE `users_token`
   ADD UNIQUE KEY `idUser` (`idUser`);
 
 --
--- Indices de la tabla `week`
---
-ALTER TABLE `week`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indices de la tabla `week_day`
---
-ALTER TABLE `week_day`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `idDay` (`idDay`) USING BTREE;
-
---
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
--- AUTO_INCREMENT de la tabla `activity`
+-- AUTO_INCREMENT de la tabla `activities`
 --
-ALTER TABLE `activity`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `activities`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `daily_activities`
+-- AUTO_INCREMENT de la tabla `game`
 --
-ALTER TABLE `daily_activities`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `game`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de la tabla `day`
+-- AUTO_INCREMENT de la tabla `game_team`
 --
-ALTER TABLE `day`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT de la tabla `month`
---
-ALTER TABLE `month`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
-
---
--- AUTO_INCREMENT de la tabla `planner`
---
-ALTER TABLE `planner`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `game_team`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `rol`
@@ -453,6 +306,12 @@ ALTER TABLE `rol`
 --
 ALTER TABLE `team`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `tipe_activity`
+--
+ALTER TABLE `tipe_activity`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `tournament`
@@ -479,41 +338,21 @@ ALTER TABLE `users_token`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT de la tabla `week`
---
-ALTER TABLE `week`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT de la tabla `week_day`
---
-ALTER TABLE `week_day`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
-
---
 -- Restricciones para tablas volcadas
 --
 
 --
--- Filtros para la tabla `daily_activities`
+-- Filtros para la tabla `activities`
 --
-ALTER TABLE `daily_activities`
-  ADD CONSTRAINT `activityID_fk_2` FOREIGN KEY (`idActivity`) REFERENCES `activity` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `dayID_fk_1` FOREIGN KEY (`idDay`) REFERENCES `day` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `activities`
+  ADD CONSTRAINT `fk_teamID_2` FOREIGN KEY (`idTeam`) REFERENCES `team` (`id`),
+  ADD CONSTRAINT `fk_tipeAct_1` FOREIGN KEY (`idTipeAct`) REFERENCES `tipe_activity` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Filtros para la tabla `planner`
+-- Filtros para la tabla `tournament`
 --
-ALTER TABLE `planner`
-  ADD CONSTRAINT `dailyAct_fk_3` FOREIGN KEY (`idDailyAct`) REFERENCES `daily_activities` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `month_fk_1` FOREIGN KEY (`idMonth`) REFERENCES `month` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `weekDay_fk_2` FOREIGN KEY (`idWeekDay`) REFERENCES `week_day` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `team`
---
-ALTER TABLE `team`
-  ADD CONSTRAINT `plannerID_fk_1` FOREIGN KEY (`idPlanner`) REFERENCES `planner` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `tournament`
+  ADD CONSTRAINT `fk_gameID_1` FOREIGN KEY (`idGame`) REFERENCES `game` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `tournament_team`
@@ -534,13 +373,6 @@ ALTER TABLE `users`
 --
 ALTER TABLE `users_token`
   ADD CONSTRAINT `users_fk_1` FOREIGN KEY (`idUser`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Filtros para la tabla `week_day`
---
-ALTER TABLE `week_day`
-  ADD CONSTRAINT `day_fk_2` FOREIGN KEY (`idDay`) REFERENCES `day` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `weekId_fk_1` FOREIGN KEY (`idWeek`) REFERENCES `week` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
