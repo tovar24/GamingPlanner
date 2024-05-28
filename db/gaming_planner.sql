3020de1720d6ee2b0850e300b386bfa6fc5a360f
tovar24 <cristian.t.12@hotmail.com>
2024-05-28 16:31:30 +0200

    New updates

--- db/gaming_planner.sql
+++ db/gaming_planner.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 16-05-2024 a las 16:19:45
+-- Tiempo de generación: 28-05-2024 a las 16:29:31
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
--- src/app/gaming-planner/planner/planner.component.ts
+++ src/app/gaming-planner/planner/planner.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-planner',
@@ -6,5 +7,24 @@ import { Component } from '@angular/core';
   styleUrl: './planner.component.css'
 })
 export class PlannerComponent {
-  public selected: any;
+  displayedColumns: string[] = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
+  plannerForm: FormGroup;
+  daysList: string[] = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes'];
+  activityList: string[] = ['VOD', 'Teórico', 'PRACC'];
+  activitiesList: string[] = [];
+
+  constructor(private fb: FormBuilder) {
+    this.plannerForm = this.fb.group({
+      day:      ['', Validators.required],
+      activity: ['', Validators.required],
+    });
+  }
+
+  addActivity() {
+
+    this.activitiesList.push(this.plannerForm.value);
+
+    console.log(this.activitiesList);
+
+  }
 }
